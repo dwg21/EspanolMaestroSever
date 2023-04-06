5ac1641787668cdb092a5477fb1d6f76f690c3f5
dwg21 <95627833+dwg21@users.noreply.github.com>
2023-04-06 17:47:21 -0500

    proxy setting

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 
-const port = process.env.PORT || 3005
+const port = process.env.PORT || 3000
 
 const start = async () => {
     try {
