13807e7e27ecbd6e95e5e4f76a79c6e8d8c185b1
dwg21 <95627833+dwg21@users.noreply.github.com>
2023-04-07 12:17:07 -0500

    logout fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -57,7 +57,9 @@ const login = async (req, res) => {
 const logout = async (req, res) => {
     res.cookie('token', 'logout', {
         httpOnly: true,
-        expires: new Date(Date.now())
+        expires: new Date(Date.now()),
+        sameSite: "None",
+        secure: true,
     });
     res.status(StatusCodes.OK).json({msg: "user logged out"})
 }
