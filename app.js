159d167dd1afcf899b7df4a9f6c1b5409af8ab46
dwg21 <95627833+dwg21@users.noreply.github.com>
2023-04-08 13:58:56 -0500

    front end build added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 require('express-async-errors'); // Dont need to use try catch in routes
 
+
 //express
 const express = require('express');
 const app = express()
@@ -8,9 +9,10 @@ const app = express()
 // rest of packages
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
-const cors = require('cors');
+// const cors = require('cors');
 
 
+const path = require('path');
 
 //database
 const connectDB = require('./db/connect');
@@ -25,18 +27,23 @@ const translationRouter = require('./routes/translationRoutes');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(express.static(path.resolve(__dirname, './build')))
+
 app.use(morgan('tiny'));
 app.use(express.json()); //logs each request to the console
 app.use(cookieParser(process.env.JWT_SECRET));
 
 
 
-app.use(cors({
-    origin:'https://espanolmaestroserver.onrender.com',
-    credentials: true,
-}))
+// app.use(cors({
+//     origin:'https://espanolmaestroserver.onrender.com',
+//     credentials: true,
+// }))
 
-app.set('trust proxy', 1);
+
+
+
 
 //route initilsiation
 app.use('/api/v1/auth',authRouter);
